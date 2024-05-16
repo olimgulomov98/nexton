41db9bb14a246ec6c3e932a65eda8a841c5e7439
olimgulomov98 <olimgulomov98@gmail.com>
2024-05-16 20:16:06 +0500

    fix: modify packages

--- src/controllers/store.controller.ts
+++ src/controllers/store.controller.ts
@@ -1,5 +1,8 @@
 import { Request, Response } from "express";
 import { T } from "../libs/types/common";
+import { MemberInput } from "../libs/types/member";
+import { MemberType } from "../libs/enums/member.enum";
+import MemberService from "../models/member.service";
 
 const storeController: T = {};
 storeController.goHome = (req: Request, res: Response) => {
@@ -23,16 +26,24 @@ storeController.getSignup = (req: Request, res: Response) => {
 storeController.getLogin = (req: Request, res: Response) => {
     try {
         console.log("getLogin");        
-        res.send("Login Page");
+        res.send("Login Page")
     } catch (err) {
-        console.log("Error, getLogin:", err);
+        console.log("Error, getLogin:", err)
     }
 }
 
-storeController.processSignup = (req: Request, res: Response) => {
+storeController.processSignup = async (req: Request, res: Response) => {
     try {
-        console.log("processSignup");        
-        res.send("DONE");
+        console.log("processSignup");
+        console.log("req.body:", req.body)
+
+        const newMember: MemberInput = req.body;
+        newMember.memberType = MemberType.STORE;
+
+        const memberService = new MemberService()
+        const result = await memberService.processSignup(newMember)
+               
+        res.send(result);
     } catch (err) {
         console.log("Error, getSignup:", err);
     }
--- src/models/member.service.ts
+++ src/models/member.service.ts
@@ -1,5 +1,31 @@
+
+import {Member, MemberInput} from "../libs/types/member";
+import Errors, { HttpCode, Message } from "../libs/Error";
+import { MemberType } from "../libs/enums/member.enum";
+import MemberModel from "../schema/member.model";
+
+
+
 class MemberService {
-    constructor() {}
+  private readonly memberModel;
+
+  constructor() {
+    this.memberModel = MemberModel
+  }
+
+  public async processSignup(input: MemberInput): Promise<Member> {
+    const exist = await this.memberModel
+      .findOne({ memberType: MemberType.STORE })
+      .exec();
+    if (exist) throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
+    try {
+      const result = await this.memberModel.create(input);
+      result.memberPassword = "";
+      return result;
+    } catch (err) {
+      throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
+    }
+  }
 }
 
-export default MemberService
+export default MemberService;
